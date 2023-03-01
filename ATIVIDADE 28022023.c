20d321048af7c32479e440032bfaebe2a51f27af
Carlos Brandão <120611117+CarlosGBrandao@users.noreply.github.com>
2023-03-01 00:01:39 -0400

    Update ATIVIDADE 28022023.c

--- ATIVIDADE 28022023.c
+++ ATIVIDADE 28022023.c
@@ -15,7 +15,7 @@ int main(int argc, char const *argv[])
         printf("\n");
     letraD(alunos, notas, sexo);
         printf("\n");
-    letraF(alunos, notas, sexo);
+    letraF(alunos, notas);
         printf("\n");
     letraG(alunos, notas, sexo);
 
